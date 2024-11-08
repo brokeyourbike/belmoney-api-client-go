6c6a506bf61441cfe0f398f7a3251cd07f229357
Ivan Stasiuk <brokeyourbike@gmail.com>
2024-11-08 22:21:00 +0000

    feat: update rate

--- outgoing.go
+++ outgoing.go
@@ -12,6 +12,7 @@ type OutgoingClient interface {
 	Processing(ctx context.Context, reference string) (ProcessingResponse, error)
 	Paid(ctx context.Context, reference, note string) (PaidResponse, error)
 	Cancel(ctx context.Context, reference, note string) (CancelResponse, error)
+	UpdateRate(ctx context.Context, reference, note string, rate float64) (UpdateRateResponse, error)
 }
 
 var _ OutgoingClient = (*client)(nil)
@@ -196,6 +197,28 @@ func (c *client) Cancel(ctx context.Context, reference, note string) (data Cance
 	}
 
 	req, err := c.newRequest(ctx, http.MethodPost, "/Cancel", cancelPayload{Reference: reference, Note: note})
+	if err != nil {
+		return data, fmt.Errorf("failed to create request: %w", err)
+	}
+
+	req.DecodeTo(&data)
+	req.ExpectStatus(http.StatusOK)
+	return data, c.do(ctx, req)
+}
+
+type UpdateRateResponse struct {
+	BaseReponse
+	Reference string `json:"Reference"`
+}
+
+func (c *client) UpdateRate(ctx context.Context, reference, note string, rate float64) (data UpdateRateResponse, err error) {
+	type cancelPayload struct {
+		Reference string  `json:"Reference"`
+		Note      string  `json:"Note"`
+		Rate      float64 `json:"Rate"`
+	}
+
+	req, err := c.newRequest(ctx, http.MethodPost, "/UpdateRate", cancelPayload{Reference: reference, Note: note, Rate: rate})
 	if err != nil {
 		return data, fmt.Errorf("failed to create request: %w", err)
 	}
